892f81f739ce57cf4b3ac8a3f90a23eeb819123b
Evan Klitzke <evan@eklitzke.org>
2012-07-30 21:36:41 -0700

    add links to file content

--- py/codesearch/server.py
+++ py/codesearch/server.py
@@ -1,10 +1,14 @@
 import optparse
-import tornado.ioloop
+import os
+
+from tornado import ioloop
+from tornado import web
 
 from codesearch import instance_watcher
 from codesearch import filesystem
 from codesearch import handler_meta
 from codesearch import handlers  # for side effects!
+from codesearch import index_pb2
 
 if __name__ == "__main__":
     parser = optparse.OptionParser()
@@ -12,22 +16,35 @@ if __name__ == "__main__":
                       help='Run the application entirely locally')
     parser.add_option('-p', '--port', type='int', default=9000,
                       help='The port to bind on')
+    parser.add_option('--index-directory', default='/tmp/index',
+                      help='The index directory to use')
     parser.add_option('--prod', dest='debug', action='store_false',
                       default=True, help='Do not run in debug mode')
     parser.add_option('--instances', type='int', default=8,
                       help='How many instances to run in prod')
     opts, args = parser.parse_args()
-    application = tornado.web.Application(
-        handler_meta.get_handlers(),
-        static_path=filesystem.get_static_dir(),
-        template_path=filesystem.get_template_dir(),
-        debug=opts.debug,
-        remote=not opts.local)
+    settings = {
+        'index_directory': opts.index_directory,
+        'static_path': filesystem.get_static_dir(),
+        'template_path': filesystem.get_template_dir(),
+        'debug': opts.debug,
+        'remote': not opts.local
+    }
+    meta_config = index_pb2.MetaIndexConfig()
+    with open(os.path.join(opts.index_directory, 'meta_config')) as f:
+        meta_config.ParseFromString(f.read())
+    vestibule = settings['vestibule'] = meta_config.vestibule
+
+    handlers = handler_meta.get_handlers()
+    handlers.append((r'/view-src/(.*)', web.StaticFileHandler,
+                     {'path': vestibule}))
+
+    application = web.Application(handlers, **settings)
 
     try:
         if opts.debug:
             application.listen(opts.port)
-            tornado.ioloop.IOLoop.instance().start()
+            ioloop.IOLoop.instance().start()
         else:
             watcher = instance_watcher.InstanceWatcher(application, opts.port)
             watcher.run(opts.instances)
